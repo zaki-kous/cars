2e8bb2a5e8c41e7d66a217b24aead411f42f41bf
zq_top <794424009@qq.com>
2018-10-27 11:39:46 +0800

    update eval sh

--- slim/finetun_inception_v3_on_cars.sh
+++ slim/finetun_inception_v3_on_cars.sh
@@ -43,7 +43,7 @@ python train_image_classifier.py \
   --checkpoint_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v3.ckpt \
   --checkpoint_exclude_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
   --trainable_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
-  --max_number_of_steps=120000 \
+  --max_number_of_steps=45000 \
   --batch_size=32 \
   --learning_rate=0.01 \
   --learning_rate_decay_type=fixed \
